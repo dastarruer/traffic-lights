ff84ce4745c6005a5c59bd09dcc0a7f932c11383
dastarruer <ayushpramanik399@gmail.com>
2025-03-06 11:53:37 -0400

    Add next function to get the next light

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,18 @@ enum TrafficLight {
     Red,
 }
 
+impl TrafficLight {
+    fn next(&self) -> Self {
+        match self {
+            TrafficLight::Green => TrafficLight::Yellow,
+            TrafficLight::Yellow => TrafficLight::Red,
+            TrafficLight::Red => TrafficLight::Green,
+        }
+    }
+}
+
 fn main() {
     let current_light = TrafficLight::Green;
     println!("Current light: {:?}", current_light);
+    println!("Next light: {:?}", current_light.next())
 }
